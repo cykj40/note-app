15b9d5d4843532148b7fb6939cfb61dddd4fded1
Cyrus Jalili Khiabani <102045473+cykj40@users.noreply.github.com>
2025-06-07 11:27:11 -0400

    webpack and babel config

---
+++ wepack.config.js
@@ -0,0 +1,53 @@
+const path = reqire("node:path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ReactServerWebpackPlugin = require("react-server-webpack-plugin");
+
+const mode = process.env.NODE_ENV || "development";
+const development = mode === "development";
+
+
+const config = {
+    mode,
+    entry: "./src/Client.jsx",
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                use: "babel-loader",
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".js", ".jsx"],
+
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            publicPath: "/assets/",
+            template: "./index.html",
+        }),
+        new ReactServerWebpackPlugin({ isServer: false }),
+
+    ],
+    output: {
+        chunkFilename: development
+            ? "[id].chunk.js"
+            : "[id].[contenthash].chunk.js",
+        path: path.resolve(__dirname, "dist"),
+        fileName: "[name].js",
+        clean: true,
+
+    },
+    optimization: {
+        runtimeChunk: "single",
+
+    }
+}
+
+module.exports = config;
