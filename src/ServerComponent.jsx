3df841a5e4a7f3bdb9066007f170b57263c057df
Cyrus Jalili Khiabani <102045473+cykj40@users.noreply.github.com>
2025-06-07 12:11:28 -0400

    created client component started ervercomponent

---
+++ src/ServerComponent.jsx
@@ -0,0 +1,10 @@
+import { AsyncDatabase } from "promised-sqlite3";
+import path from "node:path";
+
+// assumes your logged in as id 1
+export default async function MyNotes() {
+    console.log("rendering MyNotes server component");
+    async function fetchNotes() {
+        
+    }
+}
